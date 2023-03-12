a53fddf614a48aa7f06faf0ed40715d782d00003
sofia297p <sofia297484@gmail.com>
2023-03-12 22:56:46 +0200

    Revert added description

--- Words/Models/Language.cs
+++ Words/Models/Language.cs
@@ -11,15 +11,15 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
-		public String Description { get; set; }
+		
         public List<Group> Groups { get; set; }
 
-        public Language(Guid id, string title, List<Word> words, string description, List<Group> groups)
+        public Language(Guid id, string title, List<Word> words, List<Group> groups)
         {
             Id = id;
             Title = title;
             Words = words;
-			Description = description;
+			
             Groups = groups;
         }
 

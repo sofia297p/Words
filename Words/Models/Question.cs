66505111d2361d4dcca87170425c769214c7e152
sofia297p <sofia297484@gmail.com>
2023-03-12 23:15:28 +0200

    Added Question

---
+++ Words/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words.Models
+{
+    internal class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Answer { get; set; }
+    }
+}

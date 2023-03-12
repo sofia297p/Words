5ff40104ef0fe5dd9700bfd188016981f6a686db
sofia297p <sofia297484@gmail.com>
2023-03-12 18:43:30 +0200

    Fixed models

--- Words/Models/Language.cs
+++ Words/Models/Language.cs
@@ -12,6 +12,7 @@ namespace words.Models
         public string Title { get; set; }
         public List<Word> Words { get; set; }
         public String Description { get; set; }
+
         public Language(Guid id, string title, List<Word> words, string description)
         {
             Id = id;

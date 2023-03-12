2d28bda2caee71a5b02ca1b6a43976e56a159066
sofia297p <sofia297484@gmail.com>
2023-03-12 18:48:37 +0200

    Fixed models

--- Words/Models/User.cs
+++ Words/Models/User.cs
@@ -4,12 +4,16 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
         public List<Language> Languages { get; set; }
 
-        public User(Guid id, string name, List<Language> languages)
+        public User(Guid id, string name, string login, string password, List<Language> languages)
         {
             Id = id;
             Name = name;
+            Login = login;
+            Password = password;
             Languages = languages;
         }
 
